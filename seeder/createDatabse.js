5f4bb5b0a340e0441615a020d72d362cad026a45
Coli-co <awestlifeblue@gmail.com>
2023-03-19 01:41:00 +0800

    seeder modified

--- seeder/createDatabse.js
+++ seeder/createDatabse.js
@@ -1,11 +1,16 @@
-const con = require('../config/mysql')
+const connect = require('../config/mysql')
 
-con.connect(function (err) {
-  if (err) throw err
-  console.log('Connected to MySQL!')
-  con.query('CREATE DATABASE test', function (err, result) {
+function createDatabase() {
+  const con = connect()
+  con.connect(function (err) {
     if (err) throw err
-    console.log('Database created')
+    console.log('Connected to MySQL!')
+    con.query('CREATE DATABASE test', function (err, result) {
+      if (err) throw err
+      console.log('Database created')
+    })
+    con.end()
   })
-  con.end()
-})
+}
+
+createDatabase()
--- seeder/insertTable.js
+++ seeder/insertTable.js
@@ -1,14 +1,19 @@
-const con = require('../config/mysql')
+const connect = require('../config/mysql')
 
-con.connect(function (err, rows) {
-  if (err) throw err
-  console.log('Connected to MySQL!')
-  const useDatabase = `USE test`
-  const sql = `INSERT INTO admin_acc (name, password, created) VALUES ('admin', 'Admin&8181', UNIX_TIMESTAMP())`
-  con.query(useDatabase) // select database
-  con.query(sql, function (err, result) {
+function insertAdminTable() {
+  const con = connect()
+  con.connect(function (err, rows) {
     if (err) throw err
-    console.log(`admin data inserted successfully.`)
+    console.log('Connected to MySQL!')
+    const useDatabase = `USE test`
+    const sql = `INSERT INTO admin_acc (name, password, created) VALUES ('admin', 'Admin&8181', UNIX_TIMESTAMP())`
+    con.query(useDatabase) // select database
+    con.query(sql, function (err, result) {
+      if (err) throw err
+      console.log(`admin data inserted successfully.`)
+    })
+    con.end()
   })
-  con.end()
-})
+}
+
+insertAdminTable()
